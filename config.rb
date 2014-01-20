28a1e316e4c47375c508e180c4b60fb33da9f66e
Miroslav Martynov <marty.mircha@gmail.com>
2014-01-20 17:07:21 +0200

    #25: Updated foundation to 5 version. Added new mobile menu. Changed accordeon plugin for career page. Small style and settings fixes.

--- config.rb
+++ config.rb
@@ -1,5 +1,6 @@
 # Require any additional compass plugins here.
-require 'zurb-foundation'
+add_import_path "bower_components/foundation/scss"
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
